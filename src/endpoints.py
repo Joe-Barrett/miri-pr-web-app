97b391c73ecce5bbf7e8e1372b428176bca4ee8f
DHornSco <35266691+DHornSco@users.noreply.github.com>
2018-04-18 18:36:39 +0100

    Added session check for all /admin/ urls

--- src/endpoints.py
+++ src/endpoints.py
@@ -36,53 +36,65 @@ def admin_login():
 @app.route('/logout')
 def logout():
 	session['logged_in'] = False
-	return admin()
+	return redirect(url_for('admin'))
 	
 
 @app.route('/admin/contribution/new', methods=['GET', 'POST'])
 def contribution_new():
-	if request.method == 'GET':
-		return render_template('admin/contribution_form.html',
-							   endpoint=url_for('contribution_new'),
-							   map_countries=model.COUNTRIES,
-							   contributions=model.Contribution.select().order_by(model.Contribution.id)
-							   )
+	if session.get('logged_in'):
+		if request.method == 'GET':
+			return render_template('admin/contribution_form.html',
+								   endpoint=url_for('contribution_new'),
+								   map_countries=model.COUNTRIES,
+								   contributions=model.Contribution.select().order_by(model.Contribution.id)
+								   )
 
-	contribution = model.Contribution(
-		title=request.form.get('title'),
-		description=request.form.get('description'),
-		map_location=request.form.get('map_location'),
-		shown_location=request.form.get('shown_location'),
-		contributor=request.form.get('contributor'),
-	)
-	contribution.save()
+		contribution = model.Contribution(
+			title=request.form.get('title'),
+			description=request.form.get('description'),
+			map_location=request.form.get('map_location'),
+			shown_location=request.form.get('shown_location'),
+			contributor=request.form.get('contributor'),
+		)
+		contribution.save()
 
-	return redirect(url_for('contribution_edit', contr_id=contribution.id))
+		return redirect(url_for('contribution_edit', contr_id=contribution.id))
+	else:
+		return redirect(url_for('admin'))
 
 
 @app.route('/admin/contribution/<contr_id>', methods=['GET', 'POST'])
 def contribution_edit(contr_id):
-    contribution = model.Contribution.get_by_id(contr_id)
+	if session.get('logged_in'):
+		try:
+			contribution = model.Contribution.get_by_id(contr_id)
 
-    if request.method == 'GET':
-        return render_template('admin/contribution_form.html',
-                               endpoint=url_for('contribution_edit', contr_id=contr_id),
-                               map_countries=model.COUNTRIES,
-                               contribution=contribution,
-                               contributions=model.Contribution.select().order_by(model.Contribution.id)
-                               )
-    for attrib in request.form:
-        setattr(contribution, attrib, request.form.get(attrib))
-    contribution.save()
+			if request.method == 'GET':
+				return render_template('admin/contribution_form.html',
+									   endpoint=url_for('contribution_edit', contr_id=contr_id),
+									   map_countries=model.COUNTRIES,
+									   contribution=contribution,
+									   contributions=model.Contribution.select().order_by(model.Contribution.id)
+									   )
+			for attrib in request.form:
+				setattr(contribution, attrib, request.form.get(attrib))
+			contribution.save()
 
-    return redirect(url_for('contribution_edit', contr_id=contribution.id))
+			return redirect(url_for('contribution_edit', contr_id=contribution.id))
+		except:
+			return redirect(url_for('contribution_new'))
+	else:
+		return redirect(url_for('admin'))
 
 
 @app.route('/admin/contribution/<contr_id>/delete', methods=['POST'])
 def contribution_delete(contr_id):
-    model.Contribution.delete_by_id(contr_id)
+	if session.get('logged_in'):
+		model.Contribution.delete_by_id(contr_id)
 
-    return redirect(url_for('admin'))
+		return redirect(url_for('contribution_new'))
+	else:
+		return redirect(url_for('admin'))
 
 
 @app.route('/image/<img_id>', methods=['GET'])
@@ -96,34 +108,42 @@ def image(img_id):
 
 @app.route('/admin/contribution/<contr_id>/image/new', methods=['POST'])
 def image_new(contr_id):
-    image = model.Image(
-        title=request.form.get('title'),
-        description=request.form.get('description'),
-        mimetype=request.files['file'].mimetype,
-        file=request.files['file'].read(),
-        contribution=contr_id
-    )
-    image.save()
-    return redirect(url_for('contribution_edit', contr_id=contr_id))
+	if session.get('logged_in'):
+		image = model.Image(
+			title=request.form.get('title'),
+			description=request.form.get('description'),
+			mimetype=request.files['file'].mimetype,
+			file=request.files['file'].read(),
+			contribution=contr_id
+		)
+		image.save()
+		return redirect(url_for('contribution_edit', contr_id=contr_id))
+	else:
+		return redirect(url_for('admin'))
 
 
 @app.route('/admin/image/<img_id>', methods=['POST'])
 def image_edit(img_id):
-    img = model.Image.get_by_id(img_id)
-    contribution = img.contribution
-    for attrib in request.form:
-        print(request.form.get(attrib))
-        setattr(img, attrib, request.form.get(attrib))
-    img.save()
+	if session.get('logged_in'):
+		img = model.Image.get_by_id(img_id)
+		contribution = img.contribution
+		for attrib in request.form:
+			print(request.form.get(attrib))
+			setattr(img, attrib, request.form.get(attrib))
+		img.save()
 
-    return redirect(url_for('contribution_edit', contr_id=contribution.id))
-
+		return redirect(url_for('contribution_edit', contr_id=contribution.id))
+	else:
+		return redirect(url_for('admin'))
+		
 
 @app.route('/admin/image/<img_id>/delete', methods=['POST'])
 def image_delete(img_id):
-    img = model.Image.get_by_id(img_id)
-    contribution = img.contribution
-    model.Image.delete_by_id(img_id)
+	if session.get('logged_in'):
+		img = model.Image.get_by_id(img_id)
+		contribution = img.contribution
+		model.Image.delete_by_id(img_id)
 
-    return redirect(url_for('contribution_edit', contr_id=contribution.id))
-
+		return redirect(url_for('contribution_edit', contr_id=contribution.id))
+	else:
+		return redirect(url_for('admin'))
